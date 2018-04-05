49273a2a88aa81331ca3a5f77b0254b0eafc5161
Do Trung Kien <do.trung.kien@framgia.com>
2018-04-05 16:40:40 +0700

    update chapter 6

--- erc721.sol
+++ erc721.sol
@@ -1,10 +1,12 @@
-contract ERC721 {
-  event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
-  event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
+pragma solidity ^0.4.17;
 
-  function balanceOf(address _owner) public view returns (uint256 _balance);
-  function ownerOf(uint256 _tokenId) public view returns (address _owner);
-  function transfer(address _to, uint256 _tokenId) public;
-  function approve(address _to, uint256 _tokenId) public;
-  function takeOwnership(uint256 _tokenId) public;
+contract ERC721 {
+    event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
+    event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
+
+    function balanceOf(address _owner) public view returns (uint256 _balance);
+    function ownerOf(uint256 _tokenId) public view returns (address _owner);
+    function transfer(address _to, uint256 _tokenId) public;
+    function approve(address _to, uint256 _tokenId) public;
+    function takeOwnership(uint256 _tokenId) public;
 }
